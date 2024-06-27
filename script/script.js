cff8c9d53b8db0482f558c70a3eabf20fbff42e8
Paras Pandey <pppandey1123@gmail.com>
2024-06-27 13:02:43 -0400

    commir all

--- script/script.js
+++ script/script.js
@@ -1,10 +1,10 @@
 
-  document.addEventListener("DOMContentLoaded", function() {
-    const signupBtn = document.querySelector(".signup-btn");
-    const signupForm = document.getElementById("signup-form");
-  
-    signupBtn.addEventListener("click", function() {
-      signupForm.classList.remove("hidden");
-    });
+document.addEventListener("DOMContentLoaded", function() {
+  const signupBtn = document.querySelector(".signup-btn");
+  const signupForm = document.getElementById("signup-form");
+
+  signupBtn.addEventListener("click", function() {
+    signupForm.classList.remove("hidden");
   });
-    
+});
+  
